7c104f5bea272b4c281b0b5a358d839a1caad50b
daniel gordon <danielgordon19972@gmail.com>
2021-04-28 10:59:45 +0300

    about to deploy

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+require "pry"
+
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    # binding.pry
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:list_id, :movie_id, :comment)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+puts JSON.parse(open(url).read)['results'].each do |result|
+  Movie.create(title: "#{result['original_title']}", overview: "#{result['overview']}", poster_url: "https://image.tmdb.org/t/p/original#{result['poster_path']}")
+end
