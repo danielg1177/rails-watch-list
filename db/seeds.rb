86a37ed16ff374c817d9e15a917c90a06a534825
daniel gordon <danielgordon19972@gmail.com>
2021-04-28 12:58:27 +0300

    fixed issue with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'json'
 
 puts "starting seed"
 url = "http://tmdb.lewagon.com/movie/top_rated"
-puts JSON.parse(open(url).read)['results'].each do |result|
+JSON.parse(open(url).read)['results'].each do |result|
   Movie.create!(title: "#{result['original_title']}", overview: "#{result['overview']}", poster_url: "https://image.tmdb.org/t/p/original#{result['poster_path']}")
 end
 puts "finished seeding"
