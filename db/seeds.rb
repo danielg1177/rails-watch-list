21a5f4952810ad5a5c1518867881864d67feea7c
daniel gordon <danielgordon19972@gmail.com>
2021-04-28 14:40:46 +0300

    updated navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require 'json'
 puts "starting seed"
 url = "http://tmdb.lewagon.com/movie/top_rated"
 JSON.parse(open(url).read)['results'].each do |result|
-  Movie.create!(title: "#{result['original_title']}", overview: "#{result['overview']}", poster_url: "https://image.tmdb.org/t/p/original#{result['poster_path']}")
+  movie = Movie.new(title: "#{result['original_title']}", overview: "#{result['overview']}", poster_url: "https://image.tmdb.org/t/p/original#{result['poster_path']}")
+  movie.save
 end
 puts "finished seeding"
